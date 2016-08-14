11c6baff61c7e46ece156ba81676aea019951282
zhangshupeng <zhangshupeng@tuyuabc.com>
2016-08-14 10:56:54 +0800

    #modify

---
+++ luccccccc/luccccccc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  luccccccc
+//
+//  Created by zhangshupeng on 16/8/14.
+//  Copyright © 2016年 sym. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ luccccccc/luccccccc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  luccccccc
+//
+//  Created by zhangshupeng on 16/8/14.
+//  Copyright © 2016年 sym. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
